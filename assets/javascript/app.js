1c49e70a2138c39194b76f16fc2acbca8cc5eb27
Daniel Kim <danielkim@Daniels-MacBook-Air.local>
2018-07-10 14:07:01 -0400

    dynamically add pictures to card

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,14 +26,18 @@ function renderCategories(categories) {
     var deDupedCategories = [];
 
     for (var i = 0; i < categories.length; i++) {
-        dupedCategories = dupedCategories.concat( categories[i].category_path );
+        dupedCategories = dupedCategories.concat(categories[i].category_path);
     }
-    
-    for (var j = 0; j < dupedCategories.length; j++ ) {
-        if ( !deDupedCategories.includes( dupedCategories[j] ) ) {
-            deDupedCategories.push( dupedCategories[j] );
+
+    for (var j = 0; j < dupedCategories.length; j++) {
+        if (!deDupedCategories.includes(dupedCategories[j])) {
+            deDupedCategories.push(dupedCategories[j]);
         }
     }
+
+    var rightSide = deDupedCategories.splice(0, Math.floor(deDupedCategories.length / 2));
+    console.log(rightSide);
+    console.log(deDupedCategories);
 
     for (var k = 0; k < deDupedCategories.length; k++) {
         var ptag = $("<p>");
@@ -46,7 +50,42 @@ function renderCategories(categories) {
         ptag.append(label);
         $("#catContainer").append(ptag);
     }
+    for (var l = 0; l < rightSide.length; l++) {
+        var ptag = $("<p>");
+        var label = $("<label>");
+        var input = $("<input type='checkbox'>");
+        var span = $("<span>").text(rightSide[l]);
 
+        label.append(input);
+        label.append(span);
+        ptag.append(label);
+        $("#catContainerTwo").append(ptag);
+    }
+
+
+}
+
+function renderImages(image) {
+    
+    for (var i = 0; i < image.length; i++) {
+        var imageUrl = image[i].Images[0].url_570xN  
+        console.log(imageUrl);
+        var cardClass = $("<div class='card'>");
+        var cardImage = $("<div class='card-image'>");
+        var img = $("<img src=" + imageUrl + ">");
+        cardImage.append(img);
+        cardClass.append(cardImage);
+        $("#cardBox").append(cardClass);
+       
+        
+    }
+}
+
+function renderTitle(name) {
+    for (var i = 0; i < name.length; i++) {
+        var mainTitle = name[i].title
+        console.log(mainTitle);
+    }
 }
 
 
@@ -66,17 +105,52 @@ $(document).ready(function () {
                 console.log("DATA IS", data);
 
                 $("#catContainer").empty();
+                $("#catContainer2").empty();
 
                 var cat = data.results;
                 console.log(cat);
 
                 renderCategories(cat);
+                
 
                 console.log(data);
                 return (data);
             }
+            
         });
+       
+       
     });
+    $("#buttonTwo").on('click', function () {
+        $('.progress2').show(0).delay(5000).hide(0);
+        $('#thirdBox').delay(5000).show(2000);
+        
+        var terms = $('#search').val();
+        var etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords=" +
+            terms + "&limit=12&includes=Images:1&api_key=" + api_key;
+        console.log(terms);
+        $.ajax({
+            url: etsyURL,
+            dataType: 'jsonp',
+            success: function (data) {
+       
+                var cat = data.results;
+                console.log(cat);
+
+                renderImages(cat);
+                renderTitle(cat);
+                
+
+                console.log(data);
+                return (data);
+                
+            }
+            });
+    });
+    
+
+    
+
 
     //Uses categories or category chosen by user to return only items that fit the criteria ==== IN PROCESS ====
     // $('#buttonTwo').on('click', function () {
